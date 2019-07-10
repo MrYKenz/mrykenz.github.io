aa94599adfbe32b53283fbb1325db8f682546d98
MrYKenz <yasin.kenz@kcl.ac.uk>
2019-07-10 16:45:22 +0100

    Toggle through navbar

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
-// select DOM items for menu
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
-const navItems = document.querySelectorAll('.nav-item') // menu items as nodelist / array
+const navItems = document.querySelectorAll('.nav-item'); // menu items as nodelist / array
+const navItemCurrent = document.querySelector('.nav-item.current');
 
 // opening menu
-let showMenu = false; // initial state closed
+let showMenu = false; 
 
-menuBtn.addEventListener('click', menuAppear); // show menu when clicked
+menuBtn.addEventListener('click', menuAppear); 
 function menuAppear() {
-    if(!showMenu) { // add to class name for CSS
+    if(!showMenu) { // add to class name
         menuBtn.classList.add('close');
         menu.classList.add('show');
         menuNav.classList.add('show');
         menuBranding.classList.add('show');
         navItems.forEach(item => item.classList.add('show'));
         showMenu = true; 
-    } else { // remove from class name for CSS
+    } else { // remove from class name 
         menuBtn.classList.remove('close');
         menu.classList.remove('show');
         menuNav.classList.remove('show');
@@ -25,4 +25,18 @@ function menuAppear() {
         navItems.forEach(item => item.classList.remove('show'));
         showMenu = false; 
     }
+}
+
+// toggling nav item for current page
+let hovering = true;
+
+navItems.forEach(item => item.addEventListener('mouseover', moveNav));
+navItems.forEach(item => item.addEventListener('mouseout', moveBack));
+
+function moveNav() {
+    navItemCurrent.classList.remove('current');
+}
+
+function moveBack() {
+    navItemCurrent.classList.add('current');
 }
